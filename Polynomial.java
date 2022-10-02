24d61beafc95b3c605e5ac9fd5be3df1539491ea
Jitong Zhang <jitongzhang@mbp.local>
2022-10-02 17:19:27 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,27 +1,74 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Polynomial {
 	double [] coefficients;
+	int [] exponents;
 
 	public Polynomial() {
 		coefficients = new double[] {0};
+		exponents = new int [] {0};
 	}
 	
-	public Polynomial(double [] c) {
+	public Polynomial(double [] c, int[] e) {
 		coefficients = c;
+		exponents = e;
+	}
+	public Polynomial(File f){
+		String data; //not completed
+		try {
+		      Scanner reader = new Scanner(f);
+		      while (reader.hasNextLine()) {
+		    	  data = reader.nextLine();
+		      }
+		      reader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		
+	}
+	
+	public int inIntArray(int [] a, int b) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == b) return i;
+		}
+		return -1;
 	}
 	
 	public Polynomial add(Polynomial p) {
 		Polynomial result;
+		int length;
+		length = exponents.length + p.exponents.length;
+		for (int i = 0; i < exponents.length; i++) {
+			for (int j = 0; j < p.exponents.length; j++) {
+				if (exponents[i] == p.exponents[j]) {
+					length-=1;
+				}
+			}
+		}
+		result = new Polynomial(new double [length], new int [length]);
 		
-		if(p.coefficients.length > coefficients.length) {
-			result = p;
-			for (int i = 0; i<coefficients.length; i++) {
-				result.coefficients[i] += coefficients[i];
+		for(int i = 0; i < exponents.length; i++) {
+			result.exponents[i] = exponents[i];
+			result.coefficients[i] = coefficients[i];
+		}
+		int i = exponents.length;
+		while (i < length) {	
+			for (int j=0; j<p.exponents.length; j++) {
+				if (inIntArray(result.exponents,p.exponents[j]) == -1) {
+					result.exponents[i] = p.exponents[j];
+					result.coefficients[i] = p.coefficients[j];
+					i += 1;
+				}
+				else {
+					int l = inIntArray(result.exponents,p.exponents[j]);
+					result.coefficients[l] +=  p.coefficients[j];
+				}
 			}
-		} else {
-			result = this;
-			for (int i=0; i<p.coefficients.length; i++) {
-				result.coefficients[i] += p.coefficients[i];
-			}
+			
 		}
 		
 		return result;
@@ -30,7 +77,7 @@ public class Polynomial {
 	public double evaluate(double e) {
 		double result = 0;
 		for(int i=0; i<coefficients.length; i++) {
-			result += coefficients[i] * Math.pow(e, i);
+			result += coefficients[i] * Math.pow(e, exponents[i]);
 		}
 		return result;
 	}
@@ -39,4 +86,11 @@ public class Polynomial {
 		return this.evaluate(value) == 0;
 	}
 	
+	public Polynomial multiply(Polynomial p) {
+		return p; //not completed
+		
+	}
+	public void saveToFile(String filename) {
+		//not completed
+	}
 }
